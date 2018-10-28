1dadcbbec2e1c814a2bb4e275f32ecc9f1f21887
taiwogboyegun <taiwogboyegunmechack@gmail.com>
2018-10-28 04:36:41 +0100

    i created a sign in form and made a little chnages

--- src/components/ImageTodetect/ImageTodetect.js
+++ src/components/ImageTodetect/ImageTodetect.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './imageTodetect.css'
 
 
 
-const ImageTodetect = ({imageUrl, displayBox}) =>{
+const ImageTodetect = ({imageUrl, box}) =>{
     return(
       <div className='center'>
          <div className="absolute mt2">
             <img alt='image_to_detect' id="imageUrl" src={imageUrl} style={{height:'auto', width:'500px' }}/>
-            <div className='bounding_box'></div>
+            <div className='bounding_box' style={{top: box.topRow, right:box.rightCol, left:box.leftCol, bottom:box.bottomRow}}></div>
          </div>
       </div>
 
